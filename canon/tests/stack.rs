9ad22557d7365d409153f0dcbc606dfbb00268e8
Victor Lopez <victor@dusk.network>
2020-09-22 04:32:17 +0200

    Implements #11 - Canon impl for tuples up to 16

--- canon/tests/stack.rs
+++ canon/tests/stack.rs
@@ -93,4 +93,61 @@ mod test {
 
         assert_eq!(restored.pop().unwrap(), None)
     }
+
+    #[test]
+    fn multiple_restored_tuples() {
+        fn exec<F, T>(tuple: F)
+        where
+            F: Fn(u8) -> T,
+            T: core::fmt::Debug + Canon<MemStore> + Clone + Eq,
+        {
+            let store = MemStore::new();
+
+            type Int = u8;
+
+            let n: Int = 128;
+
+            let mut list = Stack::new();
+
+            for i in 0..n {
+                list.push(tuple(i));
+            }
+
+            let mut handle = Handle::new(list);
+
+            handle.commit(&store).unwrap();
+
+            let mut restored = handle.restore().unwrap();
+
+            for i in (0..n).rev() {
+                assert_eq!(restored.pop().unwrap(), Some(tuple(i)))
+            }
+
+            assert_eq!(restored.pop().unwrap(), None)
+        }
+
+        fn enforce_impl<T>(_t: T)
+        where
+            T: Canon<MemStore>,
+        {
+        }
+
+        exec(|i: u8| (i));
+        exec(|i: u8| (i, i));
+        exec(|i: u8| (i, i, i));
+        exec(|i: u8| (i, i, i, i));
+        exec(|i: u8| (i, i, i, i, i));
+        exec(|i: u8| (i, i, i, i, i, i));
+        exec(|i: u8| (i, i, i, i, i, i, i));
+        exec(|i: u8| (i, i, i, i, i, i, i, i));
+        exec(|i: u8| (i, i, i, i, i, i, i, i, i));
+        exec(|i: u8| (i, i, i, i, i, i, i, i, i, i));
+        exec(|i: u8| (i, i, i, i, i, i, i, i, i, i, i));
+        exec(|i: u8| (i, i, i, i, i, i, i, i, i, i, i, i));
+
+        enforce_impl((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2));
+        enforce_impl((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3));
+        enforce_impl((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4));
+        enforce_impl((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5));
+    }
 }
