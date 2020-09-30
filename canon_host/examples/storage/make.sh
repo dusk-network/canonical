5cdf580a8fb006847fdb19383d13b3413b8d0d31
Kristoffer Ström <kristoffer@rymdkoloni.se>
2020-09-30 16:21:50 +0200

    Deprecate Xargo, provide makefile

--- canon_host/examples/storage/make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cargo build --target wasm32-unknown-unknown --release --features hosted --color=always 2>&1 &&
-
-wasm-opt --strip --strip-producers -Oz ../../../target/wasm32-unknown-unknown/release/storage.wasm -o storage.wasm &&
-		
-stat storage.wasm | head -2
