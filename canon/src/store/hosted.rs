039cdc219dc0da68453095e26c7c13ac4d72eb88
Kristoffer Ström <kristoffer@rymdkoloni.se>
2021-02-26 03:16:50 +0100

    Further implementations of the host and bridgestore

--- canon/src/store/hosted.rs
+++ canon/src/store/hosted.rs
@@ -4,56 +4,61 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
+extern crate alloc;
+
+use alloc::vec::Vec;
+
 use crate::canon::{Canon, CanonError};
 use crate::id::Id;
 use crate::store::{Sink, Source};
 
-extern crate alloc;
-
-use alloc::vec::Vec;
+#[thread_local]
+static mut BUFFER: Vec<u8> = Vec::new();
 
 /// Store usable across ffi-boundraries
 #[derive(Clone, Copy, Default, Debug)]
 pub struct BridgeStore;
 
 impl BridgeStore {
-    fn get<T: Canon>(&self, id: &Id) -> Result<T, CanonError> {
+    pub(crate) fn get<T: Canon>(id: &Id) -> Result<T, CanonError> {
         unsafe {
             let len = id.len();
-            let mut buf = Vec::with_capacity();
-            buf.resize_with(len, || 0);
-            get(&id, &mut buf[0]);
+            // ensure we have enough space in our buffer
+            // NB: this might reallocate on growing, but never actually
+            // shrinks the capacity of the buffer
+            BUFFER.resize_with(len, || 0);
+            get(&id.bytes(), len as i32, &mut BUFFER[0]);
             // get has now written the encoded bytes of T into the buffer
-            let mut source = Source::new(&buf[..]);
+            let mut source = Source::new(&BUFFER[..]);
             T::read(&mut source)
         }
     }
 
-    fn put<T: Canon>(&self, t: &T) {
+    pub(crate) fn put<T: Canon>(t: &T) -> Id {
         unsafe {
             let len = t.encoded_len();
-            let mut buf = Vec::with_capacity();
-            buf.resize_with(len, || 0);
-            let mut sink = Sink::new(&mut buf[..]);
+            // ensure we have enough space in our buffer
+            BUFFER.resize_with(len, || 0);
+
+            let mut sink = Sink::new(&mut BUFFER[..]);
             t.write(&mut sink);
-            let mut id = Id::default();
-            put(&mut buf[0], len, &mut id.as_mut()[0]);
-            Ok(id)
+            let mut id_bytes = [0u8; 32];
+            put(&mut BUFFER[0], len as i32, &mut id_bytes);
+            Id::new(id_bytes, len)
         }
     }
 
-    fn put_raw(&self, bytes: &[u8]) {
-        unsafe {
-            let mut id = Self::Ident::default();
-            let len = bytes.len();
+    pub(crate) fn fetch(_id: &Id, _into: &mut [u8]) -> Result<(), CanonError> {
+        todo!("FIXME")
+    }
 
-            todo!()
-        }
+    pub(crate) fn id<T: Canon>(_t: &T) -> Id {
+        todo!("FIXME")
     }
 }
 
 #[link(wasm_import_module = "canon")]
 extern "C" {
-    pub fn put(buf: &mut u8, len: usize, ret: &mut u8);
-    pub fn get(buf: &mut u8);
+    pub fn put(buf: &mut u8, len: i32, ret_id_bytes: &mut [u8; 32]);
+    pub fn get(id_bytes: &[u8; 32], len: i32, buf: &mut u8);
 }
